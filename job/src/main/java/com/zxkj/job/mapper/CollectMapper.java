c268bf25b6fba05097582e8c16eca2d19eaaa849
juzhenxing <1029432816@qq.com>
2018-05-04 11:00:56 +0800

    删除job文件夹

--- job/src/main/java/com/zxkj/job/mapper/CollectMapper.java
+++
@@ -1,9 +0,0 @@
-package com.zxkj.job.mapper;
-
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-import com.zxkj.job.bean.po.CollectPo;
-
-public interface CollectMapper extends BaseMapper<CollectPo> {
-
-
-}
--- job/src/main/java/com/zxkj/job/mapper/DeliveryInformationMapper.java
+++
@@ -1,9 +0,0 @@
-package com.zxkj.job.mapper;
-
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-import com.zxkj.job.bean.po.DeliveryInformationPo;
-
-public interface DeliveryInformationMapper extends BaseMapper<DeliveryInformationPo> {
-
-
-}
